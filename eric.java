f0aba26d881d01e79a5bc77062579eafc7734dc5
kiwijuice56 <alfaroeric127@gmail.com>
2021-09-22 20:35:33 -0500

    Update eric.java

--- eric.java
+++ eric.java
@@ -1,26 +1,20 @@
 public class eric{
-    public static void main(String[] args){
-        change(45.81);
+  public static void main(String[] args){
+    change(45.81);
+    change(0);
+    change(-100);
+  }
+  public static void change(double d){
+    int[] currency = {10000, 5000, 2000, 1000, 500, 200, 100, 25, 10, 5, 1};
+    int cents = (int)(d*100);
+    for (int i = 0; i < currency.length; i++){
+      int count = 0;
+      while (cents - currency[i] >= 0){
+        count++;
+        cents -= currency[i];
+      }
+      if (count > 0)
+        System.out.println(String.format("$%.2f : %d", ((double)currency[i])/100, count) );
     }
-    public static void change(double c){
-        int[] currency = {10000, 5000, 2000, 1000, 500, 200, 100, 25, 10, 5, 1};
-        int[] count = new int[11];
-        int i = 0;
-        int d = ((int) (c*100));
-        while (d > 0){
-            if (d - currency[i] >= 0){
-                count[i]++;
-                d -= currency[i];
-            }
-            else
-                i++;
-        }
-        for (int j = 0; j < currency.length; j++){
-            if (count[j] == 0) continue;
-            if (currency[j] < 100)
-                System.out.println(currency[j] + "c - " + count[j]);
-            else
-                System.out.println("$" + currency[j]/100 + " - " + count[j]);
-        }
-    }
+  }
 }
